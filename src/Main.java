fda5c76afe87fa045edbc04108efe71b87d40a48
MazanYan <mazanyan027@gmail.com>
2019-05-04 10:11:26 +0300

    Making adequate test images and variables names in Main class

--- src/Main.java
+++ src/Main.java
@@ -8,14 +8,16 @@ public class Main {
     public static void main(String[] args) {
         int seed = new Random().nextInt();
 
-        Map s = new Map(0.5, seed);
-        s.save_heightmap("awiourespdt.png");
-        s.save_climate("Shalala.png");
-        s.save_humidity("Renji.png");
-        s.save_temperature("Bre.png");
+        Map testMap = new Map(0.5, seed);
+        testMap.save_heightmap("Heightmap.png");
+        testMap.save_climate("Climate.png");
+        testMap.save_humidity("Humidity.png");
+        testMap.save_temperature("Temperature.png");
 
-        RandomNoise renji = new RandomNoise(140,100,3,0).make_multioctaved(1).minus(1.99).minus(-1).divide(0.001);
-        renji.save_to_png("Renji Ab.png");
+        //testing noise generator to generate like resources map
+        RandomNoise noise = new RandomNoise(140,100,3,0).make_multioctaved(1).minus(1.99).minus(-1).divide(0.001);
+        noise.save_to_png("Resources.png");
+
         //testing game resources generator
         ResourcesGenerator bbblll = new ResourcesGenerator(seed);
         String[][][] sl = bbblll.toArray();
